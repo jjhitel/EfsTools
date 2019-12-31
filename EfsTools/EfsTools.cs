a1a3ecc8b15f1543a9bb206ffe0021473e41fc41
Mathias Tillman <master.homer@gmail.com>
2019-12-31 02:19:53 +0100

    Fix permissions not being carried over when uploading an mbn.

--- EfsTools/EfsTools.cs
+++ EfsTools/EfsTools.cs
@@ -369,7 +369,7 @@ namespace EfsTools
                 var b = Directory.Exists(path);
                 var tmpPath = Path.Combine(Path.GetTempPath(), "EfsTools", Path.GetRandomFileName());
                 Directory.CreateDirectory(tmpPath);
-                ExtractMbn(path, tmpPath, true);
+                ExtractMbn(path, tmpPath, false);
                 return tmpPath;
             }
 
